49a0adaaed10c711e74bbe734e8c0aa8db781634
Cristiano Mueller Kanigoski <cristianomuelllerk@gmail.com>
2026-08-27 11:50:38 -0300

    menuV1.1

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const input = document.querySelector('.barra_pesquisa input');
 const botao = document.querySelector('.barra_pesquisa button');
+const arquivoAtual = descobrirArquivoAtual(window.location.pathname);
+const opcoesMenu = document.querySelectorAll('.menu_item');
+
+function descobrirArquivoAtual(caminho) { //descobre qual arquivo esta sendo executado
+
+    caminho = caminho.split("/");
+    const ultimaPosicao = caminho.length - 1;
+
+    return caminho[ultimaPosicao];
+}
+
+opcoesMenu.forEach(function (opcao) {
+    console.log(opcao.href)
+})
 
 botao.addEventListener('click', function () {
     console.log(input.value);
@@ -8,3 +22,5 @@ botao.addEventListener('click', function () {
 
 console.log(input);
 console.log(botao);
+console.log(arquivoAtual);
+console.log(opcoesMenu);
